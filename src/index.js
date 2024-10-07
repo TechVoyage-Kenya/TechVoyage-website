620ff7023393850c6d782e05cab6070645b5fe04
Brian <brianhilsden@gmail.com>
2024-10-07 21:11:17 +0300

    added routing and setup the project for temporary gh-pages deployment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider,createBrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+
+import { routes } from './routes';
 import reportWebVitals from './reportWebVitals';
 
+//To be removed when deploying to different site. Gh-pages scripts are also to be removed
+const basename = "/TechVoyage-website";
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter(routes,{basename:basename})
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<RouterProvider router={router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
